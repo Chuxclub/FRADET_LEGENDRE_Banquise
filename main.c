012bbc2d254c67648d8288a4c56386805446cf31
Florian Legendre <florian-1992@hotmail.fr>
2020-03-13 14:22:26 +0100

    Modification mineure dans main.c pour plus de confort lors de l'utilisation des commandes administrateurs

--- main.c
+++ main.c
@@ -41,10 +41,9 @@ int main()
             if(theGame.players[i]->details.health == dead)
                 continue;
 
-            //Mise à jour du dï¿½placement du joueur par le joueur
+            //Mise à jour du deplacement du joueur par le joueur
             fflush(stdin);
             scanf("%c", &move);
-
 
             switch(move)
             {
@@ -83,15 +82,18 @@ int main()
 
             }
 
-            //Mise ï¿½ jour, par le calcul, des positions des objets mouvants (s'il n'y a pas eu de commandes admin...
-            if(move != '$')
+            //Mise a jour, par le calcul, des positions des objets mouvants (s'il n'y a pas eu de commandes admin)...
+            if(move == '$')
+                printBanquise(theGame.banquise);
+
+            else
             {
                 updateFlakes(NB_FLAKES, theGame.flakes, theGame.banquise);
                 updateHammers(NB_HAMMERS, theGame.hammers, theGame.banquise);
                 Fontebanquise(theGame.banquise);
 
-                //Rafraï¿½chissement banquise
-                //system("@cls||clear"); //Commenter cette ligne pour faciliter les tests!
+                //Rafraichissement banquise
+                system("@cls||clear"); //Commenter cette ligne pour faciliter les tests!
                 printBanquise(theGame.banquise);
             }
         }
@@ -100,47 +102,3 @@ int main()
     printf("\n Game Over ;) !\n");
     return 0;
 }
-
-
-/*
-    //Menu d'accueil, demande un nombre de joueurs
-    int nb_players = main_menu();
-    T_player **players = initPlayers(nb_players);
-
-    //Initialisation de la banquise (que de la glace)
-    T_banquise *myBanquise = initBanquise(BANQUISE_SIZE);
-
-
-    //Initialisation et ajout des objets sur la banquise
-    T_object **hammers = initHammers(NB_HAMMERS);
-    addHammers(myBanquise, hammers, NB_HAMMERS);
-
-    T_object **flakes = initFlakes(NB_FLAKES);
-    addFlakes(myBanquise, flakes, NB_FLAKES);
-
-    T_object **traps = initTraps(NB_TRAPS);
-    addTraps(myBanquise, traps, NB_TRAPS);
-
-    T_object **springs = initSprings(NB_SPRINGS);
-    addSprings(myBanquise, springs, NB_SPRINGS);
-
-
-    //VÃ©rification d'un passage de A vers B
-    T_test T = collectInfos(myBanquise, initTest(BANQUISE_SIZE));
-    T_pos *tab = initTab();
-    isRoad(T, T.posA.line, T.posA.col, tab, 0);
-
-
-    //Ajout des joueurs sur la banquise
-    addPlayers(myBanquise, players, nb_players);
-
-    //Rassemblement des parties du jeu pour les fonctions admins utiles aux tests
-    T_game_parts game_parts;
-    game_parts.banquise = myBanquise;
-    game_parts.players = players;
-    game_parts.flakes = flakes;
-    game_parts.springs = springs;
-    game_parts.traps = traps;
-//    game_parts.hammers = hammers;
-
-*/
