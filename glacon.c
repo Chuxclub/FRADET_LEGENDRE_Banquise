5ea546f640bae6318a13a3da6dabd89f9130307e
Florian Legendre <florian-1992@hotmail.fr>
2020-03-20 13:37:06 +0100

    J'ai fini de documenter glacon.c, j'ai modifié la feuille de route pour refléter les changements apportés au projet

--- glacon.c
+++ glacon.c
@@ -8,10 +8,10 @@
 
 /*
     Auteur(e)(s): Florian Legendre
-    Utilité:
-    Fonctionnement:
-    Complexité en temps (au pire):
-    Hypothèse d'amélioration possible:
+    Utilité: Meme chose que pour initTrap() dans piege.c ou initSpring() dans ressort.c, etc. En tenant compte des spécificités de l'objet glacon.
+    Fonctionnement: Meme chose que pour initTrap() dans piege.c ou initSpring() dans ressort.c, etc. En tenant compte des spécificités de l'objet glacon.
+    Complexité en temps (au pire): O(1)
+    Hypothèse d'amélioration possible: /
 */
 T_flake *initFlake()
 {
@@ -28,10 +28,10 @@ T_flake *initFlake()
 
 /*
     Auteur(e)(s): Florian Legendre
-    Utilité:
-    Fonctionnement:
-    Complexité en temps (au pire):
-    Hypothèse d'amélioration possible:
+    Utilité: Meme chose que pour initTraps() dans piege.c ou initSprings() dans ressort.c, etc.
+    Fonctionnement: Meme chose que pour initTraps() dans piege.c ou initSprings() dans ressort.c, etc.
+    Complexité en temps (au pire): O(nb_flakes)
+    Hypothèse d'amélioration possible: /
 */
 T_object **initFlakes(int nb_flakes)
 {
@@ -55,10 +55,10 @@ T_object **initFlakes(int nb_flakes)
 
 /*
     Auteur(e)(s): Florian Legendre
-    Utilité:
-    Fonctionnement:
-    Complexité en temps (au pire):
-    Hypothèse d'amélioration possible:
+    Utilité: Meme chose que pour addTraps() dans piege.c ou addSprings() dans ressort.c, etc.
+    Fonctionnement: Meme chose que pour addTraps() dans piege.c ou addSprings() dans ressort.c, etc.
+    Complexité en temps (au pire): O(banquise_size²)
+    Hypothèse d'amélioration possible: Meme chose que pour addTraps() dans piege.c ou addSprings() dans ressort.c, etc.
 */
 void addFlakes(T_banquise *banquise, T_object **flakes, int nb_flakes)
 {
@@ -99,10 +99,13 @@ void addFlakes(T_banquise *banquise, T_object **flakes, int nb_flakes)
 
 /*
     Auteur(e)(s): Florian Legendre
-    Utilité:
-    Fonctionnement:
-    Complexité en temps (au pire):
-    Hypothèse d'amélioration possible:
+    Utilité: Une acceleration correspond, en physique, a une modification du vecteur vitesse. C'est ce qu'on fait ici avec la
+             famille de fonction accelerate() et ses derivees Up, Down, Left... selon l'orientation de la variation.
+    Fonctionnement: Va chercher dans la structure le paramètre qui correspond au vecteur vitesse souhaité et lui donne une valeur
+                    constante correspondant à l'orientation voulue. Comme on veut que le glacon se déplace d'une case tous les tours,
+                    cette valeur est toujours (+/-)1.
+    Complexité en temps (au pire): O(1)
+    Hypothèse d'amélioration possible: /
 */
 void accelerateUp(T_object *bumped_flake)
 {
@@ -145,10 +148,13 @@ void stopFlake(T_object *bumped_flake)
 
 /*
     Auteur(e)(s): Florian Legendre
-    Utilité:
-    Fonctionnement:
-    Complexité en temps (au pire):
-    Hypothèse d'amélioration possible:
+    Utilité: Realiser l'interaction glacon/eau et donc la transformation de l'eau en glace et la disparition du glacon
+    Fonctionnement: Copie localement les anciennes positions du glacon. "Detruit" le glacon en supprimant son pointeur et
+                    en fixant son type a no_object. La destruction n'est cependant pas effective en memoire (cf. Hypothese
+                    d'amelioration ci-dessous)
+    Complexité en temps (au pire): O(1)
+    Hypothèse d'amélioration possible: Pour une gestion plus propre de la memoire il faudrait pouvoir free() le glacon
+                                       mais pour l'instant cela entraine des erreurs qui empechent la compilation
 */
 void BecomeIce(T_object *bumped_flake, int water_line, int water_col, T_banquise *banquise)
 {
@@ -167,10 +173,20 @@ void BecomeIce(T_object *bumped_flake, int water_line, int water_col, T_banquise
 
 /*
     Auteur(e)(s): Florian Legendre
-    Utilité:
-    Fonctionnement:
-    Complexité en temps (au pire):
-    Hypothèse d'amélioration possible:
+    Utilité: Realiser l'interaction glacon/ressort
+    Fonctionnement: Comme pour BecomeIce() on commence par copier les anciennes positions du glacons dans des variables locales.
+                    On deconnecte alors le glacon de la banquise. Ensuite on calcule immediatement de nouvelles coordonnees candidates
+                    pour le glacon (on veut qu'il n'y ait pas de delai d'un tour entre la rencontre avec le ressort et le debut de son chemin
+                    dans le sens inverse, si on ne fait pas ca alors le vecteur vitesse est certes modifie mais il faut alors attendre un second tour
+                    pour voir le glacon emprunter la direction en sens inverse. On aurait donc: 1 tour ou le glacon arrive pres du ressort, 1 tour ou
+                    son vecteur vitesse est altere et enfin au tour suivant le glacon commencerait a partir dans l'autre sens grace a UpdateFlakes().
+                    C'est ce comportement qu'on souhaite eviter).
+
+                    Avec ces nouvelles coordonnees candidates on verifie qu'il n'y a pas de joueur qui s'est place entre le ressort et le glacon entre temps.
+                    Si c'est le cas -> on tue le joueur et le glacon s'immobilise. Sinon on accelere le glacon dans le sens oppose a son sens initial et on le rebranche
+                    a la banquise a la nouvelle position.
+    Complexité en temps (au pire): O(1)
+    Hypothèse d'amélioration possible: /
 */
 void BumpSpring(T_object *bumped_flake, int neighbour_line, int neighbour_col, T_banquise *banquise)
 {
@@ -201,10 +217,10 @@ void BumpSpring(T_object *bumped_flake, int neighbour_line, int neighbour_col, T
 
 /*
     Auteur(e)(s): Florian Legendre
-    Utilité:
-    Fonctionnement:
-    Complexité en temps (au pire):
-    Hypothèse d'amélioration possible:
+    Utilité: Aiguiller selon l'objet interagissable rencontre par le glacon la procedure d'interaction a lancer
+    Fonctionnement: Aiguillage par des if/then/else et eventuellement des switch() imbriques
+    Complexité en temps (au pire): O(1) car toutes les procedures aiguillees ont au pire une complexite en O(1)
+    Hypothèse d'amélioration possible: Creer une procedure pour ameliorer la lisibilite au niveau du cas hammer_head dans le switch()
 */
 void FlakeInteraction(T_object *bumped_flake, int neighbour_line, int neighbour_col, T_banquise *banquise)
 {
@@ -269,10 +285,13 @@ void FlakeInteraction(T_object *bumped_flake, int neighbour_line, int neighbour_
 
 /*
     Auteur(e)(s): Florian Legendre
-    Utilité:
-    Fonctionnement:
-    Complexité en temps (au pire):
-    Hypothèse d'amélioration possible:
+    Utilité: Realiser pour chaque glacon du jeu une mise a jour de leurs positions dans leurs donnees internes et sur la banquise.
+             Cette fonction est utilisee dans la boucle de rafraichissement du jeu dans main.c, a chaque nouveau tour du jeu elle est donc appelee.
+    Fonctionnement: Parcourt tous les glacons dans leur tableau correspondant. Si le glacon a ete "detruit" (change en no_object, voir BecomeIce() dans ce
+                    meme module) sa mise a jour est ignoree. Dans le cas contraire on calcule le nouveau point candidat et on verifie que ce point candidat
+                    remplit certaines conditions. Si c'est le cas, soit un fait la mise a jour du glacon, soit on lance l'aiguillage des interactions.
+    Complexité en temps (au pire): O(1) car toutes les procedures que cette fonction comprend ont jusqu'ici une complexite au pire en O(1)
+    Hypothèse d'amélioration possible: /
 */
 void updateFlakes(int nb_flakes, T_object **flakes,  T_banquise *banquise)
 {
